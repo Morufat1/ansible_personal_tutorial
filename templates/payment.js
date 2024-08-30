cf398352775564168c145062eebf337fa2883470
Morufat Aminu <morufataminu7@gmail.com>
2024-08-30 20:40:57 +0000

    updates before setting up platform

--- templates/payment.js
+++
@@ -1,9 +0,0 @@
-const stripe = require('stripe')('your_stripe_secret_key');
-
-const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1099,
-    currency: 'usd',
-    payment_method_types: ['card'],
-});
-
-console.log(paymentIntent);
